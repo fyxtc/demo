b2c53a8b2f26fd867c4d7a630bb52d8bc2700411
fanyongxing <fanyongxing@qiyun.com>
2016-08-23 12:19:17 +0800

    add show troops

---
+++ Assets/Scripts/Map/TroopsStatusController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TroopsStatusController : MonoBehaviour {
+	public GameObject[] showTroops;
+	public GameObject[] prefabs;
+	// private GameObject[] showTroops = new GameObject[4]; // sorted by rank
+
+	void Awake(){
+		for(int i = 0; i < showTroops.Length; i++){
+			GameObject newObj = Instantiate(prefabs[i], showTroops[i].transform.position, showTroops[i].transform.rotation) as GameObject;
+			newObj.transform.SetParent(showTroops[i].transform.parent);
+			newObj.transform.localScale = new Vector3(15, 15, 0);
+			Destroy(showTroops[i]);
+			showTroops[i] = newObj;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
